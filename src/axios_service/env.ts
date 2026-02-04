4fa255bc3d245ad9a4d25b68f223fc98ee134a39
Ramiro Lynch <ramirolynch@hotmail.com>
2026-02-04 19:24:26 -0300

    aplicacion de variables de entorno

--- src/axios_service/env.ts
+++ src/axios_service/env.ts
@@ -1,13 +1,13 @@
 export const config = {
     APIPromocionesUrls: {
-        baseUrl: 'http://localhost:3005',
+        baseUrl: process.env.URL_MS_PROMOCIONES || 'http://localhost:3005',
         // En tu archivo de configuración de URLs
         verificarPromocionById: (clienteId: number) =>
             `/promocion/verificar-promocion?clienteId=${clienteId}`,
         getPromocionById: (id: number) => `/promocion/admin/${id}`,
     },
     APIFuncionesUrls: {
-        baseUrl: 'http://localhost:3003',
+        baseUrl: process.env.URL_MS_FUNCIONES || 'http://localhost:3003',
         obtenerPrecioEntradaByFuncionId: (id: number) =>
             `/formato/precio-entrada/${id}`,
         reservarButacasByIds: '/disponibilidad-butaca/reservar',
@@ -15,15 +15,15 @@ export const config = {
         ocuparButacasByIds: '/formato/ocupar-butacas',
     },
     APIIntegracionMPUrls: {
-        baseUrl: 'http://localhost:3007',
+        baseUrl: process.env.URL_MS_MERCADOPAGO || 'http://localhost:3007',
         abrirCobro: '/cobros/abrir-cobro',
     },
     APIEnviarMailsUrls: {
-        baseUrl: 'http://localhost:3008',
+        baseUrl: process.env.URL_MS_ENVIO_EMAILS || 'http://localhost:3008',
         sendMail: '/mails/send',
     },
     APIUsuariosUrls: {
-        baseUrl: 'http://localhost:3004',
+        baseUrl: process.env.URL_MS_USUARIOS || 'http://localhost:3004',
         getDatosClienteById: (id: number) =>
             `/usuario/admin/datos-cliente/${id}`,
     },
