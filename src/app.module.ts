9dba66f8a8713aa5eae40470b59beb7de522fb8a
Ramiro Lynch <ramirolynch@hotmail.com>
2026-02-18 19:42:51 -0300

    fix app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,11 +8,17 @@ import { Entrada } from './entities/entrada.entity';
 import { Venta } from './entities/venta.entity';
 import { EstadoVenta } from './entities/estadoVenta.entity';
 import { EstadoVentaModule } from './estado-venta/estado-venta.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
     imports: [
+        ConfigModule.forRoot({
+            isGlobal: true,
+        }),
         TypeOrmModule.forRoot({
-            port: +process.env.PUERTO_BD!,
+            type: 'postgres',
+            host: 'localhost',
+            port: Number(process.env.PUERTO_BD),
             database: process.env.PG_DATABASE_MS_USUARIOS,
             username: process.env.PG_USERNAME,
             password: process.env.PG_PASSWORD,
